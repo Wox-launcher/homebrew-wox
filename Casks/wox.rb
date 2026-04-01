7728e7a2ebb53e7c02014f892280f2a1e7890752
GitHub Action <action@github.com>
2026-04-01 01:56:02 +0000

    Update Wox cask to v

--- Casks/wox.rb
+++ Casks/wox.rb
@@ -1,16 +1,16 @@
 cask "wox" do
     arch arm: "arm64", intel: "x86_64"
     
-    version "v2.0.2"
+    version "v"
 
     on_arm do
-      sha256 "30c75c0c8a959a6589c950037d1e98c35de98c1d850924e2c4be1ff5f545779c"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.2/wox-mac-arm64.dmg",
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v/",
           verified: "github.com/Wox-launcher/Wox/"
     end
     on_intel do
-      sha256 "2485b696c4ede06bb45d257ab7931144d055728db3d8b95c7098defcc3aca01c"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.2/wox-mac-amd64.dmg",
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v/",
           verified: "github.com/Wox-launcher/Wox/"
     end
   
